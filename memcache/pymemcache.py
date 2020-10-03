f77668cdf02ffc604bac36b7b3ced0021ae12850
fturanzero <60838036+fturanzero@users.noreply.github.com>
2020-10-03 13:03:38 +0300

    Update pymemcache.py

--- memcache/pymemcache.py
+++ memcache/pymemcache.py
@@ -11,4 +11,9 @@ import pprint
 pp = pprint.PrettyPrinter(indent=4)
 pp.pprint(client.stats("cachedump","1","100"))
 
+def retrieve(key):
+  result = client.get_multi(['%s-%s' % (key, i) for i in xrange(12)])
+  serialized = ''  for i in range(12):
+    serialized=serialized+result[key+'-'+str(i)]
+    return serialize
 
